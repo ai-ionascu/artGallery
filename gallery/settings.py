860d510546dd852e854dafed21c95bdc58caeb77
ionutionascu27 <ionutionascu27@gmail.com>
2019-09-05 18:07:00 +0300

    password reset functionality with templates added

--- gallery/settings.py
+++ gallery/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER="ionutionascu27@gmail.com"
+EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
 from profiles.views import login_view, logout_view
+from profiles import url_reset
 
 
 urlpatterns = [
     url(r'^login/$', login_view, name='login'),
     url(r'^logout/$', logout_view, name='logout'),
+    url(r'^password_reset/', include(url_reset)),
 ]
