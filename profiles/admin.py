b7550f8f06e54003c15e1d56fa914c5756ff71d6
ionutionascu27 <ionutionascu27@gmail.com>
2019-09-05 14:55:57 +0300

    Profile, Address models added, Address set up as admin inline model for Profile

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Profile, Address
 
 # Register your models here.
+
+class AddressInline(admin.StackedInline):
+    model = Address
+    verbose_name = 'Address'
+    verbose_name_plural = 'Address'
+    
+class ProfileAdmin(admin.ModelAdmin):
+    inlines = [
+        AddressInline,
+        ]
+
+admin.site.register(Profile, ProfileAdmin)
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    
+    user = models.OneToOneField(User)
+    PROFILE_CHOICES = (('ARTIST', 'Artist'), ('CUSTOMER', 'Customer'))
+    profile_type = models.CharField(max_length=24, choices=PROFILE_CHOICES, blank=False, default='')
+    phone = models.CharField(max_length=24, null=False, blank=True, default='')
+
+    def __str__(self):
+        return self.user.username
+
+class Address(models.Model):
+    
+    profile = models.OneToOneField(Profile)
+    street1 = models.CharField(max_length=128, blank=True, default='')
+    street2 = models.CharField(max_length=128, blank=True, default='')
+    city = models.CharField(max_length=64, blank=True, default='')
+    county = models.CharField(max_length=64, blank=True, default='')
+    country = models.CharField(max_length=64, blank=True, default='')
+    zip_code = models.CharField(max_length=8, blank=True, default='')
