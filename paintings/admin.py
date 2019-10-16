500a0339663be38900ca4da542c0a17dac48aaf5
ionutionascu27 <ionutionascu27@gmail.com>
2019-10-16 12:14:56 +0300

    painting image preview added, kept model admin only for painting model

--- paintings/admin.py
+++ paintings/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from .models import Artist, Subject, Trend, Painting, Media
+from django.utils.html import format_html
 
-# Register your models here.
+# # Register your models here.
 
-admin.site.register(Artist)
-admin.site.register(Subject)
-admin.site.register(Trend)
-admin.site.register(Painting)
-admin.site.register(Media)
+class PaintingAdmin(admin.ModelAdmin):  
+    
+    def image_tag(self, obj):
+        return format_html('<img src="{}" alt="{}" max-width="300" height="150"/>'.format(obj.image.url, obj.name))
+
+    image_tag.short_description = 'Image Preview'
+
+    fields = ( 'name', 'artist', 'artist_user', 'image',
+             'image_tag', 'description', 'year', 'trend', 
+             'subject', 'media', 'size', 'price', 'availability' )
+    readonly_fields = ('image_tag', )
+    list_display = ['name', 'artist', 'image_tag', ]
+
+admin.site.register(Painting, PaintingAdmin)
--- paintings/models.py
+++ paintings/models.py
@@ -35,14 +35,14 @@ class Media(models.Model):
 
 class Painting(models.Model):
     name = models.CharField(max_length=128, default='')
-    artist = models.ForeignKey(Artist, blank=True)
+    artist = models.ForeignKey(Artist, blank=True, null=True)
     artist_user = models.ForeignKey(User, blank=True, null=True)
     image = models.ImageField(upload_to='images')
     description = models.TextField()
-    subject = models.ForeignKey(Subject)
+    subject = models.ForeignKey(Subject, blank=True, null=True)
     year = models.IntegerField()
-    trend = models.ForeignKey(Trend)
-    media = models.ForeignKey(Media)
+    trend = models.ForeignKey(Trend, blank=True, null=True)
+    media = models.ForeignKey(Media, blank=True, null=True)
     SIZE_CHOICES = (('SMALL', 'Small'), ('SMALL', 'Medium'), ('SMALL', 'Large'))
     size = models.CharField(max_length=24, choices=SIZE_CHOICES, blank=True, default='')
     price = models.DecimalField(max_digits=24, decimal_places=2)
