e6cf0da75cc6d5c5d40e8827e0a21812938ee6d4
ionutionascu27 <ionutionascu27@gmail.com>
2019-10-17 21:57:16 +0300

    moved context processors to home app, updated base.html navbar with db values

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 import stripe
+from paintings.models import Subject, Trend, Media, Painting
 
 def subscription_to_context(request):
 
@@ -11,3 +12,11 @@ def subscription_to_context(request):
             context = {'subscription' : subscription.cancel_at_period_end}
 
     return context
+
+def navbar(request):
+    size = Painting._meta.get_field('size').choices
+    print(size)
+    return {'subject': Subject.objects.order_by("-id")[:5], 
+            'trend': Trend.objects.order_by("-id")[:5],
+            'media': Media.objects.order_by("-id")[:5],
+            'size':size}
