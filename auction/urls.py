8a025d9a651f1dacfa8642b1124886eb6edb6eac
ionutionascu27 <ionutionascu27@gmail.com>
2020-01-14 10:56:20 +0200

    basic auctions list page created

--- auction/urls.py
+++ auction/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url, include
-from auction.views import start_auction_view
+from auction.views import list_auctions_view, start_auction_view, detail_auction_view
 
 
 urlpatterns = [
-    # url(r'^$', list_auctions_view, name='auctions'),
+    url(r'^$', list_auctions_view, name='auctions'),
+    url(r'^user/(?P<seller>\d+)/$', list_auctions_view, name='my_auctions'),
     url( r'^start/$', start_auction_view, name='start_auction'),
+    url( r'^(?P<id>\d+)/$', detail_auction_view, name='auction_detail'),
 
 ]
--- auction/views.py
+++ auction/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .models import Auction
+from paintings.models import Painting
 from .forms import AuctionForm
 import datetime
 
@@ -17,9 +19,25 @@ def start_auction_view(request):
             auction.seller = request.user
             auction.start_date = datetime.datetime.now()
             auction.save()
-
+            messages.success(request,'An auction for your painting has been successfully created.')
+            return redirect(reverse('index'))
+        else:
+            messages.error(request,"We couldn't create an auction for this painting.")
     else:
-
         auction_form = AuctionForm(request=request)
 
     return render (request, 'start_auction.html', {'auction_form' : auction_form})
+
+def list_auctions_view(request, seller=None):
+
+    auctions_list = Auction.objects.all()
+
+    if request.user.is_authenticated() and seller == request.user:
+        own_auctions_list = Auction.objects.filter(seller=request.user)
+        return render(request,  'auctions_list.html', {'own_auctions': own_auctions_list, 'seller': seller})
+
+    return render(request,  'auctions_list.html', {'auctions': auctions_list, 'seller': seller})
+
+def detail_auction_view(request, id=None):
+    auction = get_object_or_404(Auction, id=id)
+    return render(request, 'auction_detail.html', {'auction': auction})
