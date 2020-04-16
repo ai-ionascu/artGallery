cc3b1c545c9031adb1e0cf4d73745f58fc04cbd3
ionutionascu27 <ionutionascu27@gmail.com>
2020-04-16 12:56:34 +0200

    added email notification functionality when auction is finished

--- auction/scheduler.py
+++ auction/scheduler.py
@@ -1,3 +1,18 @@
+from urllib import request
+from django.core.mail import EmailMessage
+from django.template import loader
+from django.conf import settings
+
+def send_email(auction):
+
+    subject = 'Auction Finished'
+    from_email = settings.EMAIL_HOST_USER
+    to = [auction.winner.email,]
+    html_content = loader.render_to_string('auction_detail.html')
+    message = EmailMessage(subject, html_content, from_email, to)
+    message.send()
+
+
 def declare_winner(auction):
     if auction.bid_set.all():
         highest_bid = auction.bid_set.order_by('-bid')[0]
@@ -6,6 +21,8 @@ def declare_winner(auction):
             auction.winner_price = auction.current_price if auction.current_price >=auction.min_price else highest_bid.bid
             auction.save(update_fields=['winner', 'winner_price'])
             print('I have declared the winner.')
+            send_email(auction)
+            print('email sent')
         else:
             print('The minimum price has not been reached.')
     else:
