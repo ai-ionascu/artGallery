b1608185bfd61d7b2a8dd6c5db2882b71590830d
ionutionascu27 <ionutionascu27@gmail.com>
2020-06-25 15:31:20 +0200

    modified artists list, paintings list and auctions list templates, modified base template, added docstrings in base.html, created style.css file

--- auction/templatetags/custom_tags.py
+++ auction/templatetags/custom_tags.py
@@ -13,3 +13,24 @@ def live_count(qset, count=0):
         if  item.is_active:
             count+=1
     return count
+
+@register.filter()
+def live_filter(qset):
+    output=[]
+    for item in qset:
+        if  item.is_active:
+            output.append(item)
+    return output
+
+@register.filter()
+def get_remainder(count,val):
+    return count % val
+
+@register.filter()
+def is_odd_row(count,val):
+    floor = count // val
+    return floor % 2 == 0
+
+@register.filter()
+def is_last_row(count,length):
+        return count == length - 4
